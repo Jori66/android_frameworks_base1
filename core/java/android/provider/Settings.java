43b93db0960bdedf1ef10ccc81b124d72f1199c8
Lars Greiss <kufikugel@googlemail.com>
2014-11-21 22:40:46 +0100

    Frameworks: Add reboot menu - prepare for Slims Global menu - misc ch…
    …anges

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4739,6 +4739,12 @@ public final class Settings {
         public static final String SMS_DEFAULT_APPLICATION = "sms_default_application";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery and bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Name of a package that the current user has explicitly allowed to see all of that
          * user's notifications.
          *
@@ -4884,6 +4890,7 @@ public final class Settings {
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
             SLEEP_TIMEOUT
+            ADVANCED_REBOOT
         };
 
         /**
